6caeecad35a719b39583331f0992e8b4dcc0825b
ratan_g <107311447+gratann@users.noreply.github.com>
2025-01-21 14:31:44 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,6 @@ import pandas as pd
 df = pd.read_csv("spam.csv", encoding = "ISO-8859-1")
 df.head()
 
-# Dropping unnecessary columns
-df = df.drop(columns = ["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"])
-print(df.head())
+# Getting Summary of DataFrame (Number of rows and columns)
+df.info()
+df.columns
