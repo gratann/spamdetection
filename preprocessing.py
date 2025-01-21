abd3cead7a205afcb87e93001618c3cbe6775061
ratan_g <107311447+gratann@users.noreply.github.com>
2025-01-21 14:32:52 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -8,3 +8,25 @@ df.head()
 # Getting Summary of DataFrame (Number of rows and columns)
 df.info()
 df.columns
+
+# Value Counts
+value_1 = df['v1'].value_counts()
+print(value_1)
+
+# Renaming the columns for clarity
+df = df.rename(columns={"v1": "Label", "v2": "Message"})
+df.head()
+
+# Label Encoding 
+le = LabelEncoder()
+le.fit(df['Label'])
+df['encoded_label'] = le.transform(df['Label'])
+print(df)
+
+# Visualization of the spam and ham data
+
+sns.countplot(x="Label", data=df, palette ="viridis")
+plt.title("Distribution of Spam and Ham Messages")
+plt.xlabel("Label")
+plt.ylabel("Count")
+plt.show()
